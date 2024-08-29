f1ec9274af163c38fddd901e93d70626f4a0cc05
Spotika <andrey.a.cherkashin@gmail.com>
2024-08-29 14:43:21 +0300

    init: backend

---
+++ backend/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <httplib.h>
+#include <json.h>
+
+#define JSON_RESPONSE(json) res.set_content(json.dump(), "application/json")
+
+using json = nlohmann::json;
+
+int main() {
+  httplib::Server app;
+
+  app.set_post_routing_handler([](const auto& req, auto& res) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+    res.set_header("Access-Control-Allow-Headers", "*");
+  });
+
+  app.Get("/ping", [](const auto& req, auto& res) {
+    json response = {
+      {"ok", true}
+    };
+
+    JSON_RESPONSE(response);
+  });
+
+  app.listen("0.0.0.0", 8080);
+
+  return 0;
+}
