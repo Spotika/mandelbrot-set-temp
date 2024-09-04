b5e81e1260722352ca9cbd8866a3e6f17888f6aa
Spotika <andrey.a.cherkashin@gmail.com>
2024-09-04 07:46:04 +0300

    chore(frontend): vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,35 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+      // globalModulePaths: [/index\.module\.scss/]
+    }
+  },
+  build: {
+    minify: "terser",
+    terserOptions: {
+      sourceMap: true
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          lottie: ['lottie-react'],
+        }
+      },
+      plugins: [
+        {
+          name: 'disable-treeshake',
+          transform(code, id) {
+              if (id.includes("index.module.scss")) {
+                  return { moduleSideEffects: 'no-treeshake' };
+              }
+          },
+      }
+      ],
+    },
+  }
 })
+
