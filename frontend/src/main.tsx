de40ea4361a4d5f00e5d15200c636efad1efd651
Spotika <andrey.a.cherkashin@gmail.com>
2024-09-14 09:50:58 +0300

    refactor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
-import { StrictMode, useEffect, useState } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import styles from "./index.module.scss"
+// import styles from "./index.module.scss"
 import axios from 'axios';
 import { MandelbrotPage } from './pages/Mandelbrot';
 
@@ -9,9 +9,10 @@ import { MandelbrotPage } from './pages/Mandelbrot';
 axios.defaults.baseURL = process.env.NODE_ENV === "local" ? "http://localhost:8080" : "https://spotika4.difhel.dev";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <StrictMode >
     {/* <App /> */}
     {/* <HelloPage /> */}
+    <h1></h1>
     <MandelbrotPage />
   </StrictMode>
 )
